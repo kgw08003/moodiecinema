37882fd87eb129d09e1ad50d2a3924500952400b
kimjiung <kgw08003@naver.com>
2024-11-04 15:44:31 +0900

    11.04_var2

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
+# urls.py
 from django.urls import path
+from .views import SignUpView, UserProfileView, custom_logout  # custom_logout 뷰 임포트
 from django.contrib.auth.views import LoginView
-from . import views
+
 
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='moodiecinema/login.html'), name='login'),
-    path('signup/', views.SignUpView.as_view(), name='signup'),
-
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('logout/', custom_logout, name='logout'),  # custom_logout 사용
 ]
