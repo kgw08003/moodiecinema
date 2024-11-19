60172f5a1c4fa98b205487acae54a35aaa86d583
kimjiung <kgw08003@naver.com>
2024-11-19 13:31:52 +0900

    11.19_var1

--- chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import MovieChatbotHTMLView, MovieChatbotView
-
-urlpatterns = [
-    path('', MovieChatbotHTMLView.as_view(), name='chatbot'),  # 채팅봇 페이지 경로
-    path('api/', MovieChatbotView.as_view(), name='chatbot_api'),  # 챗봇 API 경로
-]
