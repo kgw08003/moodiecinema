a739ad0831abb0a145ddb91b639c396f363ee52c
kimjiung <kgw08003@naver.com>
2024-11-19 17:45:49 +0900

    11.19_var3

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,10 +1,13 @@
 # userprofile/urls.py
 from django.urls import path
-from .views import MoviesByYearAPIView, MoviesByEraAPIView, TechAdvancementsAPIView, MovieTimelineAPIView
+from .views import MoviesByYearAPIView, MoviesByEraAPIView, TechAdvancementsAPIView, MovieTimelineAPIView, GenreRatingsAPIView, RemakeMoviesAPIView, RemakeMoviesRevenueAPIView
 
 urlpatterns = [
     path('api/movies-by-year/', MoviesByYearAPIView.as_view(), name='movies_by_year'),
     path('api/movies-by-era/', MoviesByEraAPIView.as_view(), name='movies_by_era'),
     path('api/tech-advancements/', TechAdvancementsAPIView.as_view(), name='tech_advancements'),
     path('api/movie-timeline/', MovieTimelineAPIView.as_view(), name='movie_timeline'),
+    path('api/genre-ratings/', GenreRatingsAPIView.as_view(), name='genre_ratings'),
+    path('api/remake-movies/', RemakeMoviesAPIView.as_view(), name='remake_movies'),
+    path('api/remake-revenues/', RemakeMoviesRevenueAPIView.as_view(), name='remake-revenues'),
 ]
